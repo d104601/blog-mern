082fd719d08b25d60851cded746e6d0f29af6d0d
Taeyong Lee <d104601@hotmail.com>
2022-09-06 17:24:01 -0400

    working on adding new post

--- client/src/pages/NewPost.js
+++ client/src/pages/NewPost.js
@@ -3,7 +3,8 @@ import Auth from '../utils/auth';
 import { addPost } from "../utils/api";
 
 function NewPost() {
-    const [postForm, setPostForm] = useState({ title: "", username: "", content: ""})
+    const user = Auth.getProfile();
+    const [postForm, setPostForm] = useState({ title: "", username: user.data.username, content: ""})
     const [alert, setAlert] = useState(false);
 
     const inputHandler = (event) => {
@@ -12,8 +13,37 @@ function NewPost() {
     };
 
     return(
-        <div>
-
+        <div className='container mx-auto'>
+            <form className='p-4'>
+                <div className='text-xl text-center pb-4'>
+                    Create New Post
+                </div>
+                <div className='pb-4'>
+                    <label className='block'>Title</label>
+                    <input
+                    className='input'
+                    type="text"
+                    name='title'
+                    onChange={inputHandler}
+                    value={postForm.title}
+                    required>
+                    </input>
+                </div>
+                <div className='pb-4'>
+                    <label className='block'>Content</label>
+                    <textarea
+                    className='input'
+                    type="text"
+                    name='content'
+                    onChange={inputHandler}
+                    value={postForm.content}
+                    required>
+                    </textarea>
+                </div>
+            </form>
+            <div className='text-center'>
+                <button className='btn btn-outline' type='submit'>Submit</button>
+            </div>
         </div>
     );
 }
