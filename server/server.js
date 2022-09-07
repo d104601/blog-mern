d73d4095e5f2fbea378460d7a77075b8270c6e39
Taeyong Lee <d104601@hotmail.com>
2022-09-07 18:30:12 -0400

    heroku test

--- server/server.js
+++ server/server.js
@@ -10,11 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/public')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.use(routes);
