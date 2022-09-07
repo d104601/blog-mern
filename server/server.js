290689c0740e0fac53332ba89459d9653ed04d00
Taeyong Lee <d104601@hotmail.com>
2022-09-07 17:34:28 -0400

    heroku test

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.use(routes);
 
 
