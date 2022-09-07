ff515aaa7132fad1295384654d6a2d930ba58b73
Taeyong Lee <d104601@hotmail.com>
2022-09-07 18:53:56 -0400

    deployment error fix

--- server/server.js
+++ server/server.js
@@ -13,16 +13,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 app.use(routes);
 
 
 db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-    });
+    app.listen(PORT, () => console.log(`API server running on port ${PORT}!`));
 });
   
